10c43b0b4eff189c9cf063b4908ba28666ab66d1
Kaoutar <119738257+kaoutarjabri@users.noreply.github.com>
2023-06-06 20:19:07 +0200

    Add files via upload

---
+++ staticfiles/js/wishlist.js
@@ -0,0 +1,38 @@
+var updateBtns = document.getElementsByClassName('wishitem')
+
+for(var i=0; i< updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action',action)
+
+        console.log('USER :', user)
+        if (user == 'AnonymousUser'){
+            console.log('user is not autenticated')
+        }else{
+            updateUserOrder(productId,action)
+        }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('user is logged in, sending data ...')
+
+    var url = '/wish/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'content-Type':'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({'productId': productId, 'action':action})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log('data:', data)
+        location.reload() //to reload the page and see the resultes immediatly
+    })
+}
