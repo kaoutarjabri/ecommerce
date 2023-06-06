46fbf739d5ccf2c4cf9bfeddca4f28dd061ecaba
Kaoutar <119738257+kaoutarjabri@users.noreply.github.com>
2023-06-06 15:11:40 +0200

    Add files via upload

---
+++ store/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    image = models.ImageField(default='profile-user.png')
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(null=True)
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+class Product(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    category = models.ManyToManyField(Category,null=True)
+    description = models.TextField(default='description not available')
+    price= models.FloatField()
+    digital= models.BooleanField(default=False, null=True,blank=False)
+    image = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transition_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id) # id is int so we make it turn into a str
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitems_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitems_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitems_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class OrderItems(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class ShippingAddress(models.Model):
+    Customer= models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order= models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+    
+    
